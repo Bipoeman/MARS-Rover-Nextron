fca4989dfed32c54935d27596ebfab64c35aa8ff
Chanathip Yodkhuang <75182220+Bipoeman@users.noreply.github.com>
2024-10-12 11:54:47 +0700

    new parameter

--- main/background.ts
+++ main/background.ts
@@ -7,6 +7,14 @@ var net = require('net')
 var connected = false;
 var globalClient: any
 
+var keypressed = {
+  'W': 0,
+  'A': 0,
+  'S': 0,
+  'D': 0
+}
+var last_keypressed = {}
+
 const isProd = process.env.NODE_ENV === 'production'
 
 if (isProd) {
@@ -57,7 +65,6 @@ ipcMain.on("connect", async (event, connection) => {
     globalClient = client
 
     client.on('data', function (data) {
-
       console.log(data.toString());
       client.end();
     });
@@ -70,26 +77,110 @@ ipcMain.on("connect", async (event, connection) => {
       console.log('disconnected from server');
     });
   }
-  else{
+  else {
     globalClient.end()
   }
 
 })
 
-ipcMain.on("getStatus",(event,data)=>{
-  if (connected){
-    event.reply("getStatus","connected")
+ipcMain.on("getStatus", (event, data) => {
+  if (connected) {
+    event.reply("getStatus", "connected")
   }
-  else{
-    event.reply("getStatus","disconnected")
+  else {
+    event.reply("getStatus", "disconnected")
 
   }
 })
 
 ipcMain.on("keyboard", (event, key) => {
-  console.log(key)
-  if (connected) {
-    globalClient.write(JSON.stringify(key))
+  // console.log(key)
+  if ((key['key'] === "W" || key['key'] === "A" || key['key'] === "S" || key['key'] === "D")) {
+    keypressed[key['key']] = key['state']
+    if(JSON.stringify(keypressed) != JSON.stringify(last_keypressed)){
+      console.log("Sending Speed")
+      var speed = keypressToSpeed(keypressed)
+      if (connected) {
+        console.log(speed)
+        globalClient.write(JSON.stringify(speed))
+      }
+      
+      console.log(speed)
+      last_keypressed = { ...keypressed }
+    }
+  }
+  else if (!(key['key'] == "W" || key['key'] == "A" || key['key'] == "S" || key['key'] == "D")){
+    console.log(`Sending normal key ${JSON.stringify(key)}`)
+    // console.log(key)
+    if (connected) {
+      globalClient.write(JSON.stringify(key))
+    }
   }
   // if (net.connected)
 })
+
+function keypressToSpeed(keypressed){
+  console.log(keypressed)
+  let left,right
+  if (keypressed['W'] === 0 && keypressed['S'] === 0) {
+    if (keypressed['D'] === 1 && keypressed['A'] === 0) {
+      left = 150;
+      right = -150;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 0) {
+      left = -150;
+      right = 150;
+    } else if (keypressed['A'] === 0 && keypressed['D'] === 0) {
+      left = 0;
+      right = 0;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 1) {
+      left = 150;
+      right = 150;
+    }
+  } else if (keypressed['W'] === 1 && keypressed['S'] === 0) {
+    if (keypressed['D'] === 1 && keypressed['A'] === 0) {
+      left = 255;
+      right = 150;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 0) {
+      left = 150;
+      right = 255;
+    } else if (keypressed['A'] === 0 && keypressed['D'] === 0) {
+      left = 255;
+      right = 255;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 1) {
+      left = 255;
+      right = 255;
+    }
+  } else if (keypressed['S'] === 0 && keypressed['W'] === 0) {
+    if (keypressed['D'] === 1 && keypressed['A'] === 0) {
+      left = -255;
+      right = -150;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 0) {
+      left = -150;
+      right = -255;
+    } else if (keypressed['A'] === 0 && keypressed['D'] === 0) {
+      left = 0;
+      right = 0;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 1) {
+      left = -150;
+      right = -150;
+    }
+  } else if (keypressed['S'] === 1 && keypressed['W'] === 0) {
+    if (keypressed['D'] === 1 && keypressed['A'] === 0) {
+      left = -255;
+      right = -150;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 0) {
+      left = -150;
+      right = -255;
+    } else if (keypressed['A'] === 0 && keypressed['D'] === 0) {
+      left = -255;
+      right = -255;
+    } else if (keypressed['A'] === 1 && keypressed['D'] === 1) {
+      left = -255;
+      right = -255;
+    }
+  }
+  return {
+    "left":left,
+    "right":right,
+  }
+}
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
   output: 'export',
   distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
   trailingSlash: true,
