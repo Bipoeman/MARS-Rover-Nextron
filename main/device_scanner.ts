d7bd9b2c67be2ef906668b9fb739423bdab313ee
Bipoeman <75182220+Bipoeman@users.noreply.github.com>
2024-10-25 01:58:28 +0700

    wifi edition

--- main/device_scanner.ts
+++ main/device_scanner.ts
@@ -9,7 +9,7 @@ var networkInterfaces = os.networkInterfaces();
 // console.log(networkInterfaces)
 var ipaddr = []
 var macs = []
-var interfaces = networkInterfaces['Ethernet 5']
+var interfaces = networkInterfaces['Wi-Fi']
 console.log(interfaces)
 interfaces && interfaces.forEach((ip) => {
   if (ip.family == "IPv4") {
@@ -17,17 +17,18 @@ interfaces && interfaces.forEach((ip) => {
     macs.push(ip.mac)
   };
 })
-var mdns = mdnss(
-  {
-    multicast: true, // use udp multicasting
-    interface: ipaddr[0], // explicitly specify a network interface. defaults to all
-    port: 5353, // set the udp port
-    ip: '224.0.0.251', // set the udp ip
-    ttl: 255, // set the multicast ttl
-    loopback: true, // receive your own packets
-    reuseAddr: true // set the reuseAddr option when creating the socket (requires node >=0.11.13)
-  }
-)
+var mdns = mdnss()
+// var mdns = mdnss(
+//   {
+//     multicast: true, // use udp multicasting
+//     interface: ipaddr[0], // explicitly specify a network interface. defaults to all
+//     port: 5353, // set the udp port
+//     ip: '224.0.0.251', // set the udp ip
+//     ttl: 255, // set the multicast ttl
+//     loopback: true, // receive your own packets
+//     reuseAddr: true // set the reuseAddr option when creating the socket (requires node >=0.11.13)
+//   }
+// )
 
 var foundDevice = {}
 
